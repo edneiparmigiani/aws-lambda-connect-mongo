2fb41832f433db89469ada22d0981bd0f54f5d5d
Ednei Parmigiani Júnior <eparmigiani@grupomult.com.br>
2019-07-29 17:12:06 -0300

    ajusts

--- handler.js
+++ handler.js
@@ -1,24 +1,30 @@
 'use strict';
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:admin@cluster0-gnn8n.mongodb.net/test?retryWrites=true&w=majority"
+const uri = "mongodb+srv://admin:<admin>@cluster0-gnn8n.mongodb.net/test?retryWrites=true&w=majority"
 
 module.exports.connect = (event, context, callback) => {
   MongoClient.connect(uri, function (err, client) {
     if (err) {
-      console.log('error occurred while connecting to MongoDB Atlas...\n', err);
-    }
-    console.log('connected...');
+			console.log('error occurred while connecting to mongodb atlas.', err);
+		}
+		console.log('connected...');
 
     const click = {clickTime: new Date()};
     console.log(click);
 
-    const collection = client.db("test").collection('clicks').save(click, (err, result) => {
-    if (err) {
-      return console.log(err);
-    }
-      console.log('click added to db');
-    });
+    try {
+			const collection = client.db("test").collection('clicks').save(click, (err, result) => {
+				if (err) {
+					return console.log(err);
+					console.log(err);
+				}
+				console.log('click added to db');
+				res.sendStatus(201);
+			 });
+		} catch (error) {
+			console.log('error executing save record on database', error);
+		}
 
     client.close();
     console.log('conection closed...');
